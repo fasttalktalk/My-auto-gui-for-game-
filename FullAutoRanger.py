40e30c766b685c8e52eb4c0a41f307897812ca12
fasttalktalk <fasttalktalk@gmail.com>
2025-05-13 12:11:26 +0700

    Update FullAutoRanger.py

--- FullAutoRanger.py
+++ FullAutoRanger.py
@@ -1,16 +1,15 @@
 import pyautogui
 import time
 from datetime import datetime, timedelta
-from MainFunc import run_for_minutes
 
 def fullauto():
     while True:
         now = datetime.now()
 
         # รอจนถึง .00
-        next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+        next_hour = (now + timedelta(hours=1)).replace(minute=10, second=0, microsecond=0)
         if now.minute != 0 or now.second != 0:
             wait_seconds = (next_hour - now).total_seconds()
             print(f"รอจนถึงเวลา {next_hour} (อีก {wait_seconds:.0f} วินาที)")
             time.sleep(wait_seconds)
-            pyautogui.click(1,2)
+fullauto()
